d2bb956dea1639b423f900222f75b1e07dae095d
Neel Kamal <64776872+kara006n@users.noreply.github.com>
2024-03-02 00:26:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (13.32%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -11,55 +11,22 @@
 class Solution {
     public ListNode swapPairs(ListNode head) {
 
-        ListNode tempNode = head;
-        ListNode tempNode2 = head;
-        ListNode tempNode3 = null;
-        ListNode tempNode4 = null;
-        int i=0;
-
-        
-        while(tempNode2!=null){
-        	
-        	//System.out.println("temp2 value now is "+tempNode2.val);
-
-            i++;
-            if(i%2==0){
-                
-                if(i==2){
-                    head.next = tempNode2.next;
-                    tempNode2.next = head;
-                    head = tempNode2; 
-                    tempNode2 = tempNode2.next;
-                    //printLinkedList(head);
-                    //System.out.println("temp1 value from if "+tempNode.val);
-                    //System.out.println("temp2 value from if "+tempNode2.val);
-                }
-                else{
-                    tempNode3 = tempNode.next;
-                    tempNode4 = tempNode2.next;
-                    tempNode.next = tempNode2;
-                    tempNode2.next = tempNode3;
-                    tempNode3.next = tempNode4;
-                    tempNode = tempNode.next.next;
-                    tempNode2 = tempNode2.next;
-                    //printLinkedList(head);
-                    //System.out.println("temp1 value from else "+tempNode.val);
-                    
-                }
-
-                if(tempNode2==null) {
-                	continue;
-                }
-                
-                //System.out.println("temp2 value from else "+tempNode2.val);
-//                if(i!=2) {
-//                	tempNode = tempNode2.next;
-//                }
-            }
-            tempNode2 = tempNode2.next;
-        }
-
+        if(head==null||head.next==null)
         return head;
-        
+		
+		ListNode dummy = new ListNode();
+		ListNode prev = dummy;
+		ListNode curr = head;
+		
+		while(curr!=null &&  curr.next!=null) {
+			prev.next = curr.next;
+			curr.next = prev.next.next;
+			prev.next.next = curr;
+			
+			prev = curr;
+			curr = curr.next;
+		}
+		
+		return dummy.next;
     }
 }
