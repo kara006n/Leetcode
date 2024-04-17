e98cf6826626a53e78bee777ebbf8ad1adc47188
Neel Kamal <64776872+kara006n@users.noreply.github.com>
2024-04-17 19:36:54 +0530

    Time: 7 ms (45.16%) | Memory: 45.3 MB (64.94%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -1,35 +1,37 @@
 class Solution {
 
-	public List<String> letterCasePermutation(String s) {
+    public List<String> letterCasePermutation(String s) {
 
         List<String> list = new ArrayList<>();
-		String output = "";
-		recursion(s, output, 0, list);
-		return list;
+        String output = "";
+        recursion(s, output, 0, list);
+        return list;
 
-	}
+    }
 
-	private void recursion(String input, String output, int n, List<String> list) {
-		
-		if(n==input.length()) {
-			list.add(output);
-			return;
-		}
-		
-		String temp  = ""+input.charAt(n);
-		if(!temp.matches("[a-zA-Z]")) {
-			output = output + temp;
-			recursion(input, output, ++n, list);
-			return;
-		}
-		
-		String output2 = output;
-		output = output + temp.toLowerCase();
-		output2 = output2 + temp.toUpperCase();
-		n++;
-		recursion(input, output, n, list);
-		recursion(input, output2, n, list);
-		return;
+    private void recursion(String input, String output, int n, List<String> list) {
 
-	}
+        if (n == input.length()) {
+            list.add(output);
+            return;
+        }
+
+        char charAt = input.charAt(n);
+
+        if (Character.isLetter(charAt)) {
+
+            String output2 = output;
+            output = output + Character.toLowerCase(charAt);
+            output2 = output2 + Character.toUpperCase(charAt);
+            n++;
+            recursion(input, output, n, list);
+            recursion(input, output2, n, list);
+            return;
+
+        } else {
+            output = output + charAt;
+            recursion(input, output, ++n, list);
+            return;
+        }
+    }
 }
